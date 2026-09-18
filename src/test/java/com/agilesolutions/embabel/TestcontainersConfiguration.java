81ca5ec64321e5bec19ed4dc1440c8f28c1fed1b
agilesolutions <robert.rong@agile-solutions.ch>
2026-09-18 18:13:04 +0200

    Provide DataSource and NamedParameterJdbcTemplate beans in test TestcontainersConfiguration to ensure Postgres connectivity in tests

--- src/test/java/com/agilesolutions/embabel/TestcontainersConfiguration.java
+++ src/test/java/com/agilesolutions/embabel/TestcontainersConfiguration.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.grafana.LgtmStackContainer;
 import org.testcontainers.utility.DockerImageName;
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestcontainersConfiguration {
@@ -20,6 +23,22 @@ public class TestcontainersConfiguration {
 	@ServiceConnection
 	PostgreSQLContainer postgresContainer() {
 		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+	}
+
+	// Provide DataSource and NamedParameterJdbcTemplate explicitly for tests
+	@Bean
+	public DataSource dataSource(PostgreSQLContainer postgres) {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("org.postgresql.Driver");
+		ds.setUrl(postgres.getJdbcUrl());
+		ds.setUsername(postgres.getUsername());
+		ds.setPassword(postgres.getPassword());
+		return ds;
+	}
+
+	@Bean
+	public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource ds) {
+		return new NamedParameterJdbcTemplate(ds);
 	}
 
 }
