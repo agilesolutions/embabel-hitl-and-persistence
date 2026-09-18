4250056196af4983d1306d48f97e10de75214bb8
agilesolutions <robert.rong@agile-solutions.ch>
2026-09-18 18:06:42 +0200

    Make TestcontainersConfiguration public for tests

--- src/test/java/com/agilesolutions/embabel/TestcontainersConfiguration.java
+++ src/test/java/com/agilesolutions/embabel/TestcontainersConfiguration.java
@@ -8,7 +8,7 @@ import org.testcontainers.grafana.LgtmStackContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+public class TestcontainersConfiguration {
 
 	@Bean
 	@ServiceConnection
